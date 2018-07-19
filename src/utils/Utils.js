5c19a9815d7938b80381999978ba58714ed765a6
Charlie Jackson <charlie@charliejackson.com>
2018-07-19 18:50:54 +0100

    Added get project env util method

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -1,5 +1,6 @@
 const { join } = require('path');
-const { readJSON } = require('fs-extra');
+const { readJSON, pathExists } = require('fs-extra');
+const merge = require('lodash/merge');
 const globals = require('./globals');
 const runCommand = require('./runCommand');
 const git = require('./git');
@@ -60,6 +61,44 @@ class Utils {
 
   getSrcDir() {
     return this.getSrcRelativePath().then(src => this.getDestPath(src));
+  }
+
+  getProjectEnv() {
+    const envPath = this.getDestPath('.env');
+    const localEnvPath = this.getDestPath('.env.local');
+
+    const parseEnv = (file) => {
+      const env = {};
+
+      if (!file) return env;
+
+      const lines = file.split('\n');
+
+      lines.forEach((line) => {
+        const variable = line.split('=');
+
+        if (variable.length === 2) {
+          const [key, value] = variable;
+          env[key] = value;
+        }
+      });
+
+      return env;
+    };
+
+    return Promise.all([
+      this.getFileIfExists(envPath),
+      this.getFileIfExists(localEnvPath),
+    ]).then(([envContents, envLocalContents]) => {
+      const env = parseEnv(envContents);
+      const envLocal = parseEnv(envLocalContents);
+
+      return merge(env, envLocal);
+    });
+  }
+
+  getFileIfExists(path) {
+    return pathExists(path).then();
   }
 
   getSrcRelativePath() {
