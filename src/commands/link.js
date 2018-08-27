6479c67faba1e8703a6fa9c3dfd3a22f04eb7d16
Charlie Jackson <charlie@charliejackson.com>
2018-08-27 19:29:38 +0300

    Make use of the link namespaced packages module

--- src/commands/link.js
+++ src/commands/link.js
@@ -1,7 +1,15 @@
 // @flow
 
-const link = () => {
-  console.log('link time baby');
-};
+import {
+  linkAllNameSpacedDependencies,
+  getProjectDir,
+} from '@cajacko/template-utils';
+import { NPM_NAMESPACE } from '../config/general';
+
+const link = () =>
+  getProjectDir().then(projectDir =>
+    linkAllNameSpacedDependencies(NPM_NAMESPACE, projectDir));
+
+// unlink just does cd projectDir && yarn unlink all @cajacko packages && yarn install
 
 export default link;
