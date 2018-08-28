3df8869db11ef228abf313ef3ab0f63f5c8ba697
Charlie Jackson <charlie@charliejackson.com>
2018-08-28 17:35:10 +0300

    Link and unlink on run any command

--- src/utils/registerCommand.js
+++ src/utils/registerCommand.js
@@ -4,13 +4,31 @@ import {
   registerCommand as UtilsRegisterCommand,
   getProjectConfig,
   getProjectEnv,
+  getProjectDir,
+  linkAllNameSpacedDependencies,
+  unlinkAllNameSpacedDependencies,
 } from '@cajacko/template-utils';
+import { NPM_NAMESPACE } from '../config/general';
 
 const registerCommand = (command, callback, ...args) =>
   UtilsRegisterCommand(
     command,
     (...registerArgs) =>
-      Promise.all([getProjectConfig(), getProjectEnv()]).then(([projectConfig, env]) => callback(...registerArgs, projectConfig, env)),
+      Promise.all([getProjectConfig(), getProjectEnv(), getProjectDir()]).then(([projectConfig, env, projectDir]) => {
+        const run = () => callback(...registerArgs, projectConfig, env);
+
+        if (env.USE_LOCAL_LIBS) {
+          return linkAllNameSpacedDependencies(
+            NPM_NAMESPACE,
+            projectDir,
+          ).then(run);
+        }
+
+        return unlinkAllNameSpacedDependencies(
+          NPM_NAMESPACE,
+          projectDir,
+        ).then(run);
+      }),
     ...args,
   );
 
--- src/utils/watchLibIfEnabled.js
+++ src/utils/watchLibIfEnabled.js
@@ -1,17 +1,25 @@
 // @flow
 
-import { runCommandInLocalNameSpacedModules } from '@cajacko/template-utils';
+import {
+  runCommandInLocalNameSpacedModules,
+  getProjectDir,
+} from '@cajacko/template-utils';
 import { NPM_NAMESPACE } from '../config/general';
 import buildLibIfEnabled from './buildLibIfEnabled';
 
-const watchLibIfEnabled = ({ USE_LOCAL_LIBS }) => {
-  if (!USE_LOCAL_LIBS) return Promise.resolve();
+const watchLibIfEnabled = (env) => {
+  if (!env.USE_LOCAL_LIBS) return Promise.resolve();
 
-  return buildLibIfEnabled().then(() => {
-    runCommandInLocalNameSpacedModules(NPM_NAMESPACE, projectDir, 'yarn watch');
+  return buildLibIfEnabled(env).then(() =>
+    getProjectDir().then((projectDir) => {
+      runCommandInLocalNameSpacedModules(
+        NPM_NAMESPACE,
+        projectDir,
+        'yarn watch',
+      );
 
-    return Promise.resolve();
-  });
+      return Promise.resolve();
+    }));
 };
 
 export default watchLibIfEnabled;
