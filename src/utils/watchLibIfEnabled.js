ab6dfd432cba2575c2d378345e7e6c54d7ae4ef8
Charlie Jackson <charlie@charliejackson.com>
2018-08-28 17:16:53 +0300

    Added build libs command

--- src/utils/watchLibIfEnabled.js
+++ src/utils/watchLibIfEnabled.js
@@ -1,28 +1,17 @@
 // @flow
 
-import {
-  runCommandInLocalNameSpacedModules,
-  getProjectDir,
-} from '@cajacko/template-utils';
+import { runCommandInLocalNameSpacedModules } from '@cajacko/template-utils';
 import { NPM_NAMESPACE } from '../config/general';
+import buildLibIfEnabled from './buildLibIfEnabled';
 
 const watchLibIfEnabled = ({ USE_LOCAL_LIBS }) => {
   if (!USE_LOCAL_LIBS) return Promise.resolve();
 
-  return getProjectDir().then(projectDir =>
-    runCommandInLocalNameSpacedModules(
-      NPM_NAMESPACE,
-      projectDir,
-      'yarn build',
-    ).then(() => {
-      runCommandInLocalNameSpacedModules(
-        NPM_NAMESPACE,
-        projectDir,
-        'yarn watch',
-      );
+  return buildLibIfEnabled().then(() => {
+    runCommandInLocalNameSpacedModules(NPM_NAMESPACE, projectDir, 'yarn watch');
 
-      return Promise.resolve();
-    }));
+    return Promise.resolve();
+  });
 };
 
 export default watchLibIfEnabled;
