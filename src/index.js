905a750d58f70be47b4eeae2f91587cbba63954f
Charlie Jackson <charlie@charliejackson.com>
2018-08-28 17:37:38 +0300

    Log all the other commands

--- src/index.js
+++ src/index.js
@@ -5,5 +5,24 @@ import registerCommand from './utils/registerCommand';
 import start from './commands/start';
 
 registerCommand('start', start);
+registerCommand('test', () => {
+  console.log('TEST');
+});
+
+registerCommand('deploy', () => {
+  console.log('DEPLOY');
+});
+
+registerCommand('upgrade', () => {
+  console.log('UPRADE');
+});
+
+registerCommand('postinstall', () => {
+  console.log('POSTINSTALL');
+});
+
+registerCommand('precommit', () => {
+  console.log('PRECOMMIT');
+});
 
 processCommands(process.argv);
