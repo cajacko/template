22c533029b0001a9afef3325cbdc91611f7ddbfe
Charlie Jackson <charlie@charliejackson.com>
2018-08-29 10:31:43 +0300

    Updated libs

--- src/utils/unlinkLibs.js
+++ src/utils/unlinkLibs.js
@@ -4,7 +4,8 @@ import { runCommand, getProjectDir } from '@cajacko/template-utils';
 
 const unlinkLibs = () =>
   getProjectDir().then(projectDir =>
-    runCommand('yarn unlink @cajacko/template', projectDir).then(() =>
-      runCommand('yarn', projectDir)));
+    runCommand('yarn unlink @cajacko/template', projectDir)
+      .then(() => runCommand('rm -rf node_modules', projectDir))
+      .then(() => runCommand('yarn', projectDir)));
 
 export default unlinkLibs;
