6dda848ef5eeca4e74e700b2c95ce61fd9cb170c
Charlie Jackson <charlie@charliejackson.com>
2018-08-30 10:02:40 +0300

    Shilfted the old files

---
+++ src/commands/register.js
@@ -0,0 +1,33 @@
+// @flow
+
+import { processCommands } from '@cajacko/template-utils';
+import registerCommand from '../utils/registerCommand';
+import upgrade from './upgrade';
+
+registerCommand('start', () => {
+  console.log('START');
+});
+
+registerCommand('test', () => {
+  console.log('TEST');
+});
+
+registerCommand('deploy', () => {
+  console.log('DEPLOY');
+});
+
+registerCommand('upgrade', upgrade);
+
+registerCommand(
+  'postinstall',
+  () => {
+    console.log('POSTINSTALL');
+  },
+  { ignoreUnlink: true },
+);
+
+registerCommand('precommit', () => {
+  console.log('PRECOMMIT');
+});
+
+processCommands(process.argv);
--- src/index.js
+++ src/index.js
@@ -1,33 +1,3 @@
 // @flow
 
-import { processCommands } from '@cajacko/template-utils';
-import registerCommand from './utils/registerCommand';
-import upgrade from './commands/upgrade';
-
-registerCommand('start', () => {
-  console.log('START');
-});
-
-registerCommand('test', () => {
-  console.log('TEST');
-});
-
-registerCommand('deploy', () => {
-  console.log('DEPLOY');
-});
-
-registerCommand('upgrade', upgrade);
-
-registerCommand(
-  'postinstall',
-  () => {
-    console.log('POSTINSTALL');
-  },
-  { ignoreUnlink: true },
-);
-
-registerCommand('precommit', () => {
-  console.log('PRECOMMIT');
-});
-
-processCommands(process.argv);
+import './commands/register';
