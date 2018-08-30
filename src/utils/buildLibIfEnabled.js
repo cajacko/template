cbab1231366a565d16c4c7a9bb649e7cc88db85b
Charlie Jackson <charlie@charliejackson.com>
2018-08-30 09:55:14 +0300

    Log build and hide the progress

--- src/utils/buildLibIfEnabled.js
+++ src/utils/buildLibIfEnabled.js
@@ -3,14 +3,26 @@
 import {
   runCommandInLocalNameSpacedModules,
   getProjectDir,
+  logger,
 } from '@cajacko/template-utils';
 import { NPM_NAMESPACE } from '../config/general';
 
 const buildLibIfEnabled = ({ USE_LOCAL_LIBS }) => {
   if (!USE_LOCAL_LIBS) return Promise.resolve();
 
-  return getProjectDir().then(projectDir =>
-    runCommandInLocalNameSpacedModules(NPM_NAMESPACE, projectDir, 'yarn build'));
+  logger.debug('Using local libs');
+  logger.debug('- Building local libs');
+  return getProjectDir()
+    .then(projectDir =>
+      runCommandInLocalNameSpacedModules(
+        NPM_NAMESPACE,
+        projectDir,
+        'yarn build',
+        { noLog: true },
+      ))
+    .then(() => {
+      logger.debug('- Finished building local libs');
+    });
 };
 
 export default buildLibIfEnabled;
