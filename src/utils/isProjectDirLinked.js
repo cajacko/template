0776883b2a16e525bb02bc2a52a4192a868f7429
Charlie Jackson <charlie@charliejackson.com>
2018-08-31 16:45:17 +0300

    Ensure lib is linked for isLinked to be true

--- src/utils/isProjectDirLinked.js
+++ src/utils/isProjectDirLinked.js
@@ -10,8 +10,9 @@ const isProjectDirLinked = () =>
       Promise.all([
         isSymLinked(join(projectDir, 'node_modules/@cajacko/template')),
         isSymLinked(join(__dirname, '../../node_modules/@cajacko/template-utils')),
+        isSymLinked(join(projectDir, 'node_modules/@cajacko/lib')),
       ]))
-    .then(([link1, link2]) => link1 && link2)
+    .then(([link1, link2, link3]) => link1 && link2 && link3)
     .catch(() => true);
 
 export default isProjectDirLinked;
