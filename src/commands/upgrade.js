77d35e21b8391adab2d2f01f614af2015d3319ea
Charlie Jackson <charlie@charliejackson.com>
2018-09-14 20:41:41 +0100

    Fixed upgrade command

--- src/commands/upgrade.js
+++ src/commands/upgrade.js
@@ -7,7 +7,7 @@ import {
   getSettings,
   git,
   runCommand,
-  getProjectDir
+  getProjectDir,
 } from '@cajacko/template-utils';
 import { readJSON } from 'fs-extra';
 import { join } from 'path';
@@ -19,7 +19,7 @@ const updatePackage = (
   updatePackageWithNewVersionDir,
   dontAdd
 ) =>
-  getShouldUpdatePackage(packageDir).then(shouldUpdatePackage => {
+  getShouldUpdatePackage(packageDir).then((shouldUpdatePackage) => {
     if (!shouldUpdatePackage) return Promise.resolve();
 
     return askForNewPackageVersion(packageDir).then(version =>
@@ -27,8 +27,7 @@ const updatePackage = (
         .then(() => git.commit(packageDir, `v${version}`))
         .then(() =>
           // Does this need to do a github one as well
-          git.tag(packageDir, `v${version}`, `Published version ${version}`)
-        )
+          git.tag(packageDir, `v${version}`, `Published version ${version}`))
         .then(() => git.push(packageDir))
         .then(() => {
           if (dontAdd) return Promise.resolve();
@@ -40,49 +39,40 @@ const updatePackage = (
             git.commit(
               updatePackageWithNewVersionDir,
               `Upgraded ${packageName} lib to v${version}`
-            )
-          );
-        })
-    );
+            ));
+        }));
   });
 
 const upgrade = () =>
-  Promise.all([getSettings(['localNPMPackagePaths']), getProjectDir()]).then(
-    ([localNPMPackagePaths, projectDir]) => {
-      return readJSON(join(projectDir, 'package.json')).then(
-        ({ dependencies }) => {
-          const templateUtilsDir =
-            localNPMPackagePaths['@cajacko/template-utils'];
-          const templateDir = localNPMPackagePaths['@cajacko/template'];
-          const libDir = localNPMPackagePaths['@cajacko/lib'];
+  Promise.all([getSettings(['localNPMPackagePaths']), getProjectDir()]).then(([localNPMPackagePaths, projectDir]) =>
+    readJSON(join(projectDir, 'package.json')).then(({ dependencies }) => {
+      const templateUtilsDir =
+          localNPMPackagePaths['@cajacko/template-utils'];
+      const templateDir = localNPMPackagePaths['@cajacko/template'];
+      const libDir = localNPMPackagePaths['@cajacko/lib'];
 
-          return updatePackage(
-            templateUtilsDir,
-            'https://github.com/cajacko/template-utils.git',
-            'template-utils',
-            templateDir
-          )
-            .then(() =>
-              updatePackage(
-                libDir,
-                'https://github.com/cajacko/lib.git',
-                'lib',
-                projectDir,
-                !!dependencies['@cajacko/lib']
-              )
-            )
-            .then(() =>
-              updatePackage(
-                templateDir,
-                'https://github.com/cajacko/template.git',
-                'template',
-                projectDir,
-                !!dependencies['@cajacko/template']
-              )
-            );
-        }
-      );
-    }
-  );
+      return updatePackage(
+        templateUtilsDir,
+        'https://github.com/cajacko/template-utils.git',
+        'template-utils',
+        templateDir
+      )
+        .then(() =>
+          updatePackage(
+            libDir,
+            'https://github.com/cajacko/lib.git',
+            'lib',
+            projectDir,
+            !dependencies['@cajacko/lib']
+          ))
+        .then(() =>
+          updatePackage(
+            templateDir,
+            'https://github.com/cajacko/template.git',
+            'template',
+            projectDir,
+            !dependencies['@cajacko/template']
+          ));
+    }));
 
 export default upgrade;
