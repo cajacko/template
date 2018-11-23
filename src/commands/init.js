58f9f30c599751bac71e0e348191864960fb2ee1
Charlie Jackson <charlie@charliejackson.com>
2018-11-23 12:25:54 +0000

    🌴 feat(github): Ask if branch permissions are setup correctly
    - Original Branch: master

--- src/commands/init.js
+++ src/commands/init.js
@@ -5,35 +5,51 @@ import {
   getProjectConfig,
   getLastLocalModuleVersion,
   logger,
+  ask,
 } from '@cajacko/template-utils';
 import { copy } from 'fs-extra';
 import { join } from 'path';
 import SetupRunner from '../modules/SetupRunner';
 
+/**
+ * Initialise the project
+ *
+ * @return {Promise} Resolves when the initialisation has finished
+ */
 const init = () =>
-  Promise.all([getProjectDir(), getProjectConfig()]).then(([projectDir, projectConfig]) => {
-    if (!projectConfig) {
-      logger.log('No project.json found in the project dir. Creating one now.');
+  Promise.all([getProjectDir(), getProjectConfig()])
+    .then(([projectDir, projectConfig]) => {
+      if (!projectConfig) {
+        logger.log('No project.json found in the project dir. Creating one now.');
 
-      return copy(
-        join(__dirname, '../../files/example/project.json'),
-        join(projectDir, 'project.json'),
-      ).then(() => {
-        logger.log('Created project.json. Please check it is accurate before running "init" again');
-      });
-    }
+        return copy(
+          join(__dirname, '../../files/example/project.json'),
+          join(projectDir, 'project.json')
+        ).then(() => {
+          logger.log('Created project.json. Please check it is accurate before running "init" again');
+        });
+      }
 
-    return getLastLocalModuleVersion('@cajacko/template').then(lastTemplateVersion =>
-      getLastLocalModuleVersion('@cajacko/lib').then((lastLibVersion) => {
-        const setupRunner = new SetupRunner(
-          projectDir,
-          projectConfig,
-          lastTemplateVersion,
-          lastLibVersion,
-        );
+      return getLastLocalModuleVersion('@cajacko/template').then(lastTemplateVersion =>
+        getLastLocalModuleVersion('@cajacko/lib').then((lastLibVersion) => {
+          const setupRunner = new SetupRunner(
+            projectDir,
+            projectConfig,
+            lastTemplateVersion,
+            lastLibVersion
+          );
 
-        return setupRunner.runSteps();
-      }));
-  });
+          return Promise.resolve();
+          return setupRunner.runSteps();
+        }));
+    })
+    .then(() =>
+      ask([
+        {
+          type: 'confirm',
+          message: 'Do you have branch permission setup correctly in GitHub',
+          name: 'branchProtection',
+        },
+      ]));
 
 export default init;
